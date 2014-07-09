ee6a54be350dd53cfcc7f3cac8dfcf41981c6f18
Konstantin Malanchev <the+git@homb.it>
2014-07-09 23:41:57 +0400

    Clearify code of exhibitions app

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -5,6 +5,3 @@ from exhibitions.models import Exhibition
 
 class ExhibitionsListView(ListView):
 	model = Exhibition
-	def get_context_data(self, **kwargs):
-		context = super(ExhibitionsListView, self).get_context_data(**kwargs)
-		return context
