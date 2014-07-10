d31f449c74b60f0a980098f66b46bad6e9ab7da0
Konstantin Malanchev <the+git@homb.it>
2014-07-10 19:05:39 +0400

    Fix gellries look up: now galleries show only paintings that they content

--- paintings/urls.py
+++ paintings/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from paintings.views import PaintingDetailView, PaintingsListView
+from paintings.views import GalleryDetailView, PaintingDetailView
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-	url( r'^(?P<gallery>\w+)/$', PaintingsListView.as_view(), name='gallery' ),
+	url( r'^(?P<slug>\w+)/$', GalleryDetailView.as_view(), name='gallery' ),
 	url( r'^(?P<gallery>\w+)/(?P<pk>\d+)/$', PaintingDetailView.as_view() ),
 )
--- paintings/views.py
+++ paintings/views.py
@@ -3,13 +3,20 @@ from django.views.generic import DetailView, ListView
 from paintings.models import Gallery, Painting
 
 
+class GalleryDetailView(DetailView):
+	model = Gallery
+	slug_field = 'title_en'
+
+	def get_context_data(self, **kwargs):
+		context = super(GalleryDetailView, self).get_context_data(**kwargs)
+		context['paintings'] = Painting.objects.filter(gallery=self.get_object())
+		#print(gallery)
+		return context
+
+
 class GalleryListView(ListView):
 	model = Gallery
 
 
 class PaintingDetailView(DetailView):
-	model = Painting
-
-
-class PaintingsListView(ListView):
 	model = Painting
