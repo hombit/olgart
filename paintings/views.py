fdcf03c8d8c38e9b4f655ba36b4d365bbf8616b7
Konstantin Malanchev <the+git@homb.it>
2014-07-10 19:10:09 +0400

    Replace code that shows gallery list in navbar to base.html

--- paintings/views.py
+++ paintings/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from paintings.models import Gallery, Painting
 
@@ -10,12 +10,7 @@ class GalleryDetailView(DetailView):
 	def get_context_data(self, **kwargs):
 		context = super(GalleryDetailView, self).get_context_data(**kwargs)
 		context['paintings'] = Painting.objects.filter(gallery=self.get_object())
-		#print(gallery)
 		return context
-
-
-class GalleryListView(ListView):
-	model = Gallery
 
 
 class PaintingDetailView(DetailView):
