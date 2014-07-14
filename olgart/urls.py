771270576a86d55cf618de2defe821e73da986ea
Konstantin Malanchev <the+git@homb.it>
2014-07-14 20:22:25 +0400

    Initial version of feedback page

--- olgart/urls.py
+++ olgart/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', 'olgart.views.index', name='index'),
+	url(r'^feedback/', 'olgart.views.feedback', name='feedback'),
 	
 	url( r'^exhibitions/', include('exhibitions.urls') ),
 	url( r'^galleries/', include('paintings.urls') ),
---
+++ static/js/feedback.js
@@ -0,0 +1,10 @@
+// KAMENT
+var kament_subdomain = 'olgart';
+var kament_page_name = 'feedback';
+
+(function() {
+	var node = document.createElement('script'); node.type = 'text/javascript'; node.async = true;
+	node.src = 'http://' + kament_subdomain + '.svkament.ru/js/embed.js';
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(node);
+})();
+// /KAMENT
