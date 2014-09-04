fe7e82257703e3c5773d3e321435cb4443eed46d
Konstantin Malanchev <the+git@homb.it>
2014-09-04 16:36:57 +0400

    Add gunicorn configuration file

---
+++ gunicorn_config.py
@@ -0,0 +1,8 @@
+import multiprocessing, os.path
+
+
+command = '/usr/local/bin/gunicorn'
+pythonpath = os.path.dirname( os.path.abspath(__file__) )
+bind = '127.0.0.1:9000'
+workers = multiprocessing.cpu_count() * 2 + 1
+user = 'nobody'
