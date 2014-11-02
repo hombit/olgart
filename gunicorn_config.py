02c8636cb534e5e365e993687dcd4bccc7bc1506
Konstantin Malanchev <hombit@Hombits-Macbook.local>
2014-11-02 17:46:35 +0300

    Minor update of gunicorn config file

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,7 @@
 import multiprocessing, os.path
 
-
 command = '/usr/local/bin/gunicorn'
 pythonpath = os.path.dirname( os.path.abspath(__file__) )
 bind = '127.0.0.1:9000'
 workers = multiprocessing.cpu_count() * 2 + 1
-user = 'nobody'
+user = 'olgart'
