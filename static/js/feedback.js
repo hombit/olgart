50cb0f4c2c9162201dd96954517697c267176824
Konstantin Malanchev <hombit@gmail.com>
2016-08-12 13:02:39 +0300

    https for feedback

--- static/js/feedback.js
+++ static/js/feedback.js
@@ -4,7 +4,7 @@ var kament_page_name = 'feedback';
 
 (function() {
 	var node = document.createElement('script'); node.type = 'text/javascript'; node.async = true;
-	node.src = 'http://' + kament_subdomain + '.svkament.ru/js/embed.js';
+	node.src = 'https://' + kament_subdomain + '.svkament.ru/js/embed.js';
 	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(node);
 })();
 // /KAMENT
